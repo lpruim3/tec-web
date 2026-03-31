f363d5971cba9c1e066548d3ae53cb2fb60ab981
lpruim3 <lpruim3@gmail.com>
2026-03-31 13:16:18 -0300

    a

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,20 @@
 import './style.css'
+const form = document.getElementById("formLogin");
+
+if (form) {
+  const email = document.getElementById("email");
+  const senha = document.getElementById("senha");
+  const erro = document.getElementById("erro");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (email.value === "" || senha.value === "") {
+      erro.textContent = "Preencha todos os campos";
+      return;
+    }
+
+    erro.textContent = "";
+    alert("Login realizado com sucesso!");
+  });
+}
